d2470a22b5d9a0e567287a8dde0343a23929a5c2
w-chiang <williampwc@gmail.com>
2015-01-06 17:10:37 -0800

    edited Student.php

--- Student.php
+++ Student.php
@@ -29,16 +29,18 @@ class Student {
     
     function average() {
         $total = 0;
-        foreach( $this->grades as $value)
+        foreach( $this->grades as $value){
             $total += $value;
+        }
         return $total/count($this->grades);
     }
     
     function toString() {
         $result = $this->first_name .''. $this->surname;
         $result .='('.$this->average().")\n";
-        foreach($this->emails as $which=>$what)
+        foreach($this->emails as $which=>$what) {
             $result .=$which .':'. $what. "\n";
+        }
         $result .="\n";
         return '<pre>'.$result.'</pre>';
     }
