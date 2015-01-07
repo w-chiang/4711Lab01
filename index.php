af798e41a143a1a27a45ebeee7050b9aa9c67619
w-chiang <williampwc@gmail.com>
2015-01-06 17:25:24 -0800

    sorted students before printing them out

--- index.php
+++ index.php
@@ -36,6 +36,8 @@ and open the template in the editor.
         $second->add_grade(50);
         $students['a456'] = $second;
 
+        ksort($students);
+        
         foreach ($students as $student) {
             echo $student->toString();
         }
